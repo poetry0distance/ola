b5ec559fafffca7c93d1ac0d1c6f5049e3a767d5
王涛 <wt283983@alibaba-inc.com>
2021-05-21 17:24:39 +0800

    feature:add some question answer

---
+++ src/main/java/com/demo/leetcode/MinArray.java
@@ -0,0 +1,22 @@
+package com.demo.leetcode;
+
+/**
+ * <p>Title: EMP </p>
+ * Description: <br>
+ * Copyright: CorpRights terminus.io<br>
+ * Company: terminus.io<br>
+ *
+ * @author wang.tao
+ * @version 1.0-SNAPSHOT
+ * @date 2021-05-21
+ *
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2]
+ * 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
+ */
+public class MinArray {
+
+    public int minArray(int[] ints){
+
+        return 0;
+    }
+}
--- src/main/java/com/demo/leetcode/NumWays.java
+++ src/main/java/com/demo/leetcode/NumWays.java
@@ -11,9 +11,16 @@ package com.demo.leetcode;
  * @date 2021-05-21
  * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
  * 斐波拉契数列解法
+ *
+ *
  */
 public class NumWays {
 
+    /**
+     * 这种解题思路一点都没有体现
+     * @param num
+     * @return
+     */
     public int numWays(int num){
         int c = 1,nxt = 1;
         for (int i=0;i<num;i++){
